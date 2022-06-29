a25f5d669693f2bf5dbe38e058ad1033e72ccad1
Jhon_Alván <alvann1995@hotmail.com>
2022-06-28 20:08:12 -0500

    washku1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # washku
+# washku git init git add README.md git commit -m first commit git branch -M main git remote add origin https://github.com/Jhonnixonalvan/washku.git git push -u origen principal
+# washku
---
+++ index.js
@@ -0,0 +1,57 @@
+
+    window.onload = function(){
+    const slider = document.querySelector("#slider");
+    let slider__images = document.querySelectorAll(".slider__images");
+    let slider__imagesLast = slider__images[slider__images.length - 1];
+    
+    const btnLeft = document.querySelector("#btn__left");
+    const btnRight = document.querySelector("#btn__right");
+    
+    slider.insertAdjacentElement('afterbegin', slider__imagesLast);
+    
+    function moverDerecha () {
+        let slider__imagesFirst = document.querySelectorAll(".slider__images")[0];
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "all 0.5s";
+        setTimeout(function(){
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('beforeend', slider__imagesFirst);
+            slider.style.marginLeft = "-100%";
+        }, 500)
+    };
+    
+    function moverIzquierda () {
+        let slider__images = document.querySelectorAll(".slider__images");
+        let slider__imagesLast = slider__images[slider__images.length - 1];
+        slider.style.marginLeft = "0%";
+        slider.style.transition = "all 0.5s";
+        setTimeout(function(){
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('afterbegin', slider__imagesLast);
+            slider.style.marginLeft = "-100%";
+        }, 500)
+    };
+    
+    btnRight.addEventListener('click', function(){
+        moverDerecha();
+    });
+    
+    btnLeft.addEventListener('click', function(){
+        moverIzquierda();
+    });
+    
+    setInterval(function(){
+        moverDerecha();
+    }, 5000);
+    }
+// ---------Serach----------------------------------
+function mostrar_buscador() {
+ document.getElementById("ctn_search").style.display= "block";
+ document.getElementById("ctn_search").style.position= "absolute";
+ document.getElementById("ctn_search").style.zIndex= "20";
+ document.getElementById("ctn_search").style.Transition = 'all 1000ms';
+}
+function  cerrar_buscador() {
+    document.getElementById("ctn_search").style.display= "none";
+}
+
