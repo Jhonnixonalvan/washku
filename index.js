dbb7544fd15d817c83f38ab000a3fbe33251156d
Jhon_Alván <alvann1995@hotmail.com>
2022-07-15 14:01:18 -0500

    cambios para entregar

--- index.js
+++ index.js
@@ -56,7 +56,96 @@
     }, 5000);
     }
 
+
+
+
+
+
+
+
+
     // ---carrito----
+    let allcontainerCart = document.querySelector(".cards");
+    let buyThings = [];
+    let containerBuyCart = document.querySelector(".mycart");
+    loadEvents();
+    function loadEvents() {
+        allcontainerCart.addEventListener('click', addProduct);
+
+        containerBuyCart.addEventListener("click", deleteProduct);
+    }
+
+    function addProduct(e) {
+        e.preventDefault();
+        if(e.target.classList.contains("addToCart")) {
+            const selectProduct = e.target.parentElement;
+            const selectProduct2 = selectProduct.parentElement; 
+            readTheContent(selectProduct2);
+        }
+        
+    }
+
+    function deleteProduct(e) {
+        if(e.target.classList.contains("cartIconElm")) {
+            console.log(e.target.classList.contains("cartIconElm"));
+
+            // const deleteId = e.target.getAtribute("id")
+            // buyThings = buyThings.filter(product =>product.id != deleteId )        
+        }
+        loadHtml();
+    }
+
+
+    function readTheContent(product) {
+        const infoProduct = {
+            id   : product.closest(".card1"),
+            image: product.querySelector("#imgs").src,
+            title: product.querySelector(".soldp1").textContent,
+            price: product.querySelector(".soldbuyP1 span").textContent,
+            amount: 1
+            
+        }
+
+        buyThings = [...buyThings, infoProduct]
+        loadHtml();
+
+    }
 
     
+    function loadHtml() {
+        clearhtml();
+        buyThings.forEach(ele => {
+            const {id, image, title, price, amount} = ele;
+            const row = document.createElement("section");
+            row.classList.add("MycartItems");
+            row.innerHTML = `
+            <section class="mycartItems">
+            <div class="cartclose">
+                <img class="cartcloseimg" src="assets/icon/cerrar.png" alt="">
+            </div>
+            <div class="mycarcontimg">
+                <img class="cartimg1" src="${image}" alt="">
+            </div>
+            <div class="cartdetails">
+                <h4 class="carttitle1">${title}</h4>
+                <h5 class="cartprice">$ <span>${price}</span> <h5>
+                <h6 class="cartstock">amount: 50</h6>
+                <div class="cart__amount">
+                    <div class="cart__amount-content">
+                      <span class="cart__amount-box minus"data-id="2">-</span>                
+                      <span class="cart__amount-number">${amount} units</span>
+                      <span class="cart__amount-box plus"data-id="1">+</span>
+                    </div>
+                    <img class="cartIconElm" data-id="1" src="assets/icon/eliminar.png" alt="">
+                </div>
+            </div>
+        </section>
+            `;
 
+            containerBuyCart.appendChild(row);
+        })
+    }
+
+    function clearhtml() {
+        containerBuyCart.innerHTML = "";
+    }
